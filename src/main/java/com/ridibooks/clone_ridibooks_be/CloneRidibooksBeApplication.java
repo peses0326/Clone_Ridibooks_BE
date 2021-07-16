07d0e9485ac4dc1453b05040e358870db7109857
Gorani92 <peses0326@gmail.com>
2021-07-16 16:29:09 +0900

    Create Repository

---
+++ src/main/java/com/ridibooks/clone_ridibooks_be/CloneRidibooksBeApplication.java
@@ -0,0 +1,13 @@
+package com.ridibooks.clone_ridibooks_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneRidibooksBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloneRidibooksBeApplication.class, args);
+    }
+
+}
