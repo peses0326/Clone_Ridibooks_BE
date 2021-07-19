cb3253f284201ad74bc710849d3bbb382eb00e8b
Gorani92 <peses0326@gmail.com>
2021-07-19 18:11:36 +0900

    Swagger 세팅

--- src/main/java/com/ridibooks/clone_ridibooks_be/controller/CommentController.java
+++ src/main/java/com/ridibooks/clone_ridibooks_be/controller/CommentController.java
@@ -4,11 +4,14 @@ import com.ridibooks.clone_ridibooks_be.dto.CommentRequestDto;
 import com.ridibooks.clone_ridibooks_be.model.Comment;
 import com.ridibooks.clone_ridibooks_be.repository.CommentRepository;
 import com.ridibooks.clone_ridibooks_be.service.CommentService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags = {"2. Commet_댓글api"}) // Swagger # 2. Comment 댓글
 @RequiredArgsConstructor
 @RestController
 public class CommentController {
@@ -17,18 +20,21 @@ public class CommentController {
     private final CommentService commentService;
 
     // 댓글 전체 조회
+    @ApiOperation(value="전체 댓글 조회", notes="모든 댓글을 조회합니다.")
     @GetMapping("/comment")
     public List<Comment> getAllComment() {
         return commentRepository.findAllByOrderByIdDesc();
     }
 
     // 책별 댓글 조회
+    @ApiOperation(value="책별 댓글 조회", notes="책별 댓글을 조회합니다.")
     @GetMapping("/comment/{bookId}")
     public List<Comment> getComment(@PathVariable Long bookId) {
         return commentRepository.findAllByBookIdOrderByCreatedAtDesc(bookId).orElseThrow(() -> new IllegalArgumentException("해당 bookId가 없습니다."));
     }
 
     // 댓글 생성
+    @ApiOperation(value="댓글 생성", notes="해당 댓글을 생성합니다.")
     @PostMapping("/comment")
     public Comment createComment(@RequestBody CommentRequestDto requestDto) {
         Comment comment = new Comment(requestDto);
@@ -36,12 +42,14 @@ public class CommentController {
     }
 
     // 댓글 수정
+    @ApiOperation(value="댓글 수정", notes="해당 댓글을 수정합니다.")
     @PutMapping("/comment/{id}")
     public Comment updateComment(@PathVariable Long id, @RequestBody CommentRequestDto requestDto) {
         return commentService.update(id, requestDto);
     }
 
     // 댓글 삭제
+    @ApiOperation(value="댓글 삭제", notes="해당 댓글을 삭제합니다.")
     @DeleteMapping("/comment/{id}")
     public void deleteComment(@PathVariable Long id) {
         commentRepository.deleteById(id);
--- src/main/java/com/ridibooks/clone_ridibooks_be/controller/UserController.java
+++ src/main/java/com/ridibooks/clone_ridibooks_be/controller/UserController.java
@@ -5,6 +5,8 @@ import com.ridibooks.clone_ridibooks_be.model.User;
 import com.ridibooks.clone_ridibooks_be.repository.UserRepository;
 import com.ridibooks.clone_ridibooks_be.security.JwtTokenProvider;
 import com.ridibooks.clone_ridibooks_be.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+@Api(tags = {"3. User_회원 관리"}) // Swagger # 3. 회원 관리
 @RequiredArgsConstructor
 @RestController
 public class UserController {
@@ -24,17 +27,13 @@ public class UserController {
     private final UserRepository userRepository;
 
     // 회원 가입 요청 처리
+    @ApiOperation(value="회원 가입 요청", notes="회원 가입 요청합니다.")
     @PostMapping("/user/signup")
     public void registerUser(@Valid @RequestBody SignupRequestDto requestDto) {
         userService.registerUser(requestDto);
     }
 
-    @GetMapping("/user/kakao/callback")
-    public void kakaoLogin(String code) {
-        // authorizedCode: 카카오 서버로부터 받은 인가 코드
-        userService.kakaoLogin(code);
-    }
-
+    @ApiOperation(value="로그인 요청", notes="로그인 요청합니다.")
     @PostMapping("/user/login")
     public String login(@RequestBody SignupRequestDto requestDto) {
         User user = userRepository.findByUsername(requestDto.getUsername())
@@ -43,5 +42,12 @@ public class UserController {
             throw new IllegalArgumentException("잘못된 비밀번호입니다.");
         }
         return jwtTokenProvider.createToken(user.getUsername(), user.getRole());
+    }
+
+    @ApiOperation(value="카카오 소셜 로그인", notes="카카오 소셜 로그인합니다.")
+    @GetMapping("/user/kakao/callback")
+    public void kakaoLogin(String code) {
+        // authorizedCode: 카카오 서버로부터 받은 인가 코드
+        userService.kakaoLogin(code);
     }
 }
